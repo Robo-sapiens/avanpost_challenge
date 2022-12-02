3106b6f7f5253eab3acf55eee00254a2090dab4c
Nojpg <dream.doomwalker@gmail.com>
2022-12-03 02:32:22 +0300

    Add gen inputjson flag

--- main.go
+++ main.go
@@ -5,20 +5,46 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 var jsonFilePath string
+var generateJsonFlag bool
 
 func init() {
-  flag.StringVar(&jsonFilePath, "file", "", "path")
+	flag.StringVar(&jsonFilePath, "file", "", "path")
+	flag.BoolVar(&generateJsonFlag, "gen-json", false, "generate json")
+}
+
+func generateJson(relPath string, input string) {
+	absPath, _ := filepath.Abs(relPath)
+
+	files, err := ioutil.ReadDir(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  jsonData := map[string]string{}
+	for i, file := range files {
+		fieldName := fmt.Sprintf("image_%d", i)
+		jsonData[fieldName] = file.Name()
+	}
+
+	jsonFile, _ := json.MarshalIndent(jsonData, "  ", "")
+	ioutil.WriteFile(input, jsonFile, 0644)
 }
 
 func main() {
 
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Printf("jsonFilePath: %v\n", jsonFilePath)
+	fmt.Printf("jsonFilePath: %v\n", jsonFilePath)
+
+	if generateJsonFlag {
+		generateJson("SOCOFing/Altered/Altered-Easy", jsonFilePath)
+	}
 
 	jsonFile, err := os.Open(jsonFilePath)
 	defer jsonFile.Close()
